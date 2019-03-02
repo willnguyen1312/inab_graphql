218101ec8e23023ae7a53899adeba35b0bb3fdb9
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-03-02 14:55:48 +0700

    Prepare docker for heroku container deployment

--- packages/server/src/utils/createTypeormConn.ts
+++ packages/server/src/utils/createTypeormConn.ts
@@ -1,13 +1,12 @@
 import { createConnection, getConnectionOptions } from 'typeorm';
-import { IS_PROD } from '../config';
 import { User } from '../entity/User';
 
 export const createTypeormConn = async () => {
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
-  (await IS_PROD)
+  return process.env.NODE_ENV === 'production'
     ? createConnection({
         ...connectionOptions,
-        url: process.env.DATABASE_URL as string,
+        url: process.env.DATABASE_URL,
         entities: [User],
         name: 'default',
       } as any).then(conn => conn.runMigrations())

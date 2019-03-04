e9e0bfdbde2a80e13fd339c46592ec405cac8e1b
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-03-04 20:33:35 +0700

    Add simple dynamic navbar - logout button

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,16 +1,9 @@
 import { Link, navigate, Router } from '@reach/router';
-import ApolloClient, { gql, InMemoryCache } from 'apollo-boost';
-import { SERVER_URL } from 'config';
+import { gql } from 'apollo-boost';
 import React from 'react';
-import { ApolloProvider } from 'react-apollo';
+import { ApolloConsumer, graphql } from 'react-apollo';
 import { Flex } from 'rebass';
 import AuthRoute from './modules/auth/AuthRoute';
-
-const client = new ApolloClient({
-  uri: SERVER_URL,
-  credentials: 'include',
-  cache: new InMemoryCache(),
-});
 
 const Home = React.lazy(() => import('./pages/Home'));
 const Login = React.lazy(() => import('./pages/Login'));
@@ -38,39 +31,58 @@ const NavLink = props => (
   />
 );
 
-const App = () => (
-  <ApolloProvider client={client}>
-    <Flex pl={15} pt={15} flexDirection="column">
-      <Flex>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/me">Me</NavLink>
-        <NavLink to="/login">Login</NavLink>
-        <NavLink to="/register">Register</NavLink>
-        <button
-          style={{
-            cursor: 'pointer',
-          }}
-          onClick={async () => {
-            await client.mutate({
-              mutation: LogoutMutation,
-            });
-            client.resetStore();
-            navigate('/login');
-          }}
-        >
-          Logout
-        </button>
-      </Flex>
-      <React.Suspense fallback={'Loading...'}>
-        <Router>
-          <Home path="/" />
-          <Login path="/login" />
-          <Register path="/register" />
-          <AuthRoute as={Me} path="/me" />
-        </Router>
-      </React.Suspense>
-    </Flex>
-  </ApolloProvider>
-);
+const meQuery = gql`
+  query MeQuery {
+    me {
+      email
+    }
+  }
+`;
 
-export default App;
+const App = props => {
+  const {
+    data: { loading, me },
+  } = props;
+
+  const isAuth = Boolean(me);
+  return loading ? null : (
+    <ApolloConsumer>
+      {client => (
+        <Flex pl={15} pt={15} flexDirection="column">
+          <Flex>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/me">Me</NavLink>
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/register">Register</NavLink>
+            {isAuth && (
+              <button
+                style={{
+                  cursor: 'pointer',
+                }}
+                onClick={async () => {
+                  await client.mutate({
+                    mutation: LogoutMutation,
+                  });
+                  client.resetStore();
+                  navigate('/login-');
+                }}
+              >
+                Logout
+              </button>
+            )}
+          </Flex>
+          <React.Suspense fallback={'Loading...'}>
+            <Router>
+              <Home path="/" />
+              <Login path="/login" />
+              <Register path="/register" />
+              <AuthRoute as={Me} path="/me" />
+            </Router>
+          </React.Suspense>
+        </Flex>
+      )}
+    </ApolloConsumer>
+  );
+};
+
+export default graphql(meQuery)(App);
--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,9 +1,25 @@
+import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-boost';
+import { SERVER_URL } from 'config';
 import React from 'react';
+import { ApolloProvider } from 'react-apollo';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+  link: new HttpLink({
+    uri: SERVER_URL,
+    credentials: 'include',
+  }),
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
 
 if (module.hot) {
   module.hot.accept();
